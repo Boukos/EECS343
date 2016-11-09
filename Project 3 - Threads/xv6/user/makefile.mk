379cdcdb4396362121af87322a4b307d61ab25e5
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-11-08 23:53:39 -0600

    Project 3 - Threads

--- Project 3 - Threads/xv6/user/makefile.mk
+++ Project 3 - Threads/xv6/user/makefile.mk
@@ -19,7 +19,23 @@ USER_PROGS := \
 	zombie\
 	badclone\
 	clone\
+	clone2\
+	clone3\
+	# cond\
+	# cond2\
+	# cond3\
 	# join\
+	# join2\
+	# join3\
+	# join4\
+	# locks\
+	# multi\
+	# noexit\
+	# race\
+	# size\
+	stack\
+	# thread\
+	# thread2\
 
 
 USER_PROGS := $(addprefix user/, $(USER_PROGS))
