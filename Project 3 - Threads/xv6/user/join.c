f209e6440136b1ab6ee495447272d5bc578c86b3
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-11-11 22:18:54 -0600

    Project 3 - Threads

--- Project 3 - Threads/xv6/user/join.c
+++ Project 3 - Threads/xv6/user/join.c
@@ -32,15 +32,9 @@ main(int argc, char *argv[])
 
    int arg = 42;
    int new_thread_pid = clone(worker, &arg, stack);
-   // printf(1, "return value = %d\n", *(uint*)(stack + PGSIZE - 8));
    assert(new_thread_pid > 0);
-   printf(1, "stack = %d\n", (int)stack);
-   printf(1, "ustack = %d\n", find_ustack(new_thread_pid));
-   int ustack = find_ustack(new_thread_pid);
-   assert((int)stack == ustack);
-   free((void*)ustack);
+
    int join_pid = join(new_thread_pid);
-   printf(1, "return value = %d\n", *(uint*)(stack + PGSIZE - 8));
    assert(join_pid == new_thread_pid);
    assert(global == 2);
 
@@ -56,3 +50,4 @@ worker(void *arg_ptr) {
    global++;
    exit();
 }
+
